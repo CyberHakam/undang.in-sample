e9a99769de70214bf0f0be274078c1a8658c5a07
IndraWijaya <59533246+CyberHakam@users.noreply.github.com>
2026-01-12 21:15:12 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-function openInvitation() {
-    document.querySelector('.cover').style.display = 'none';
-    document.getElementById('content').classList.remove('hidden');
-  
-    const music = document.getElementById('bg-music');
+const music = document.getElementById("music");
+
+/* ================= BUKA UNDANGAN ================= */
+function openInvite() {
+  document.querySelector(".cover").style.display = "none";
+  document.getElementById("content").classList.remove("hidden");
+  music.play(); // autoplay musik setelah klik
+}
+
+/* ================= TOGGLE MUSIC ================= */
+function toggleMusic() {
+  if (music.paused) {
     music.play();
+  } else {
+    music.pause();
   }
-  
+}
